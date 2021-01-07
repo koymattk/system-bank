2ca9218842340b9454910e0035c6438f37020d21
Gabriel <koymattk@gmail.com>
2021-01-07 18:04:43 -0300

    Project created

---
+++ backend/src/main/java/com/gabriel/guilherme/systembank/SystemBankApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.guilherme.systembank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabriel/guilherme/systembank/SystemBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.guilherme.systembank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
