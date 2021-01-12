4bf3191cfa63b46c4b0da86c9abc95ecb797974d
Guilherme Ilan <gui.tioze@gmail.com>
2021-01-12 15:42:17 -0300

    alterações

---
+++ frontend/bank-front/src/Pages/Main/Styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 20%;
+    
+`
+
+export const Text = styled.div`
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    padding: 2px;
+`
+
+export const Inputs = styled.div`
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    
+    input {
+        padding: 10px;
+        margin: 10px;
+        width: 200px;
+    }
+
+    button{
+        margin: 10px;
+        padding: 10px;
+        width: 225px;
+    }
+
+`
