6a2a4c19b4018809aa12367bbe288807448696d1
Gabriel <koymattk@gmail.com>
2021-01-14 13:54:20 -0300

    integrating Registerpage with the backend

--- frontend/bank-front/src/Pages/RegisterPage/index.js
+++ frontend/bank-front/src/Pages/RegisterPage/index.js
@@ -1,30 +1,68 @@
-import React, { useState } from 'react';
-import * as S from './styles';
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import { postClient , validate } from '../../Services/api';
+import './style.css';
 
- 
-export default function ()  {
+
+
+function RegisterPage() {
     const[clientType, setClientType] = useState(0);
+    const[email, setEmail] = useState('');
+    const[login, setLogin] = useState('');
+    const[password, setPassword] = useState('');
+    const[cPassword, setCPassword] = useState('');
+    const[name, setName] = useState('');
+    const[tell,setTell] = useState('');
+    const[cpf, setCpf] = useState('');
+    const[rg, setRg] = useState('');
+    const[client, setClient] = useState();
+
+    useEffect(()=>{
+        if(client === undefined)
+            alert("Popule os campos abaixo para o Cadastro");
+        else
+            postClient(client);
+    },[client])
+    
     return(
-        <S.Container>
-        <form>
-            <div>
+        <form className="registerContainer">
+            <fieldset className="registerContent">
+                <legend>CADASTRO BANK</legend>
                 <label>Login</label>
-                <input type="text" placeholder="Login Desejado" />
+                <input value = {login} onChange={login => setLogin(login.target.value)} type="text" placeholder="Login Desejado" />
+                <label>Email</label>
+                <input type="text" value = {email} placeholder="email" onChange={email => setEmail(email.target.value)}/>
                 <label>Senha</label>
-                <input type="text" placeholder="Senha" />
+                <input value={password} onChange={password => setPassword(password.target.value)} type="password" placeholder="Senha" />
                 <label>Senha</label>
-                <input type="text" placeholder="confirme sua senha" />
+                <input value={cPassword} onChange={cPassword => setCPassword(cPassword.target.value)} type="password" placeholder="confirme sua senha" />
+                <label>nome</label>
+                <input type="text" value={name} placeholder="nome" onChange={name => setName(name.target.value)}/>
+                <label>Tell:</label>
+                <input type="text" value={tell} placeholder="telefone" onChange={tell => setTell(tell.target.value)} />
                 <label>CPF</label>
-                <input type="text" placeholder="Digite seu CPF" />
+                <input value={cpf} onChange={cpf => setCpf(cpf.target.value)} type="text" placeholder="Digite seu CPF" />
                 <label>RG</label>
-                <input type="text" placeholder="Digite seu RG" />
+                <input value={rg} onChange={rg => setRg(rg.target.value)} type="text" placeholder="Digite seu RG" />
                 <label>opções de conta</label>
                 <select value={clientType} onChange={e => setClientType(e.target.value)}>
-                    <option value={0} key="">Cliente</option>
-                    <option value={1} key="">Empregador</option>
+                    <option value={0} >Cliente</option>
+                    <option value={1} >Empregador</option>
                 </select>
-            </div>
+                <button type="button" onClick={()=>{
+                    if(validate(login,password,cPassword,cpf,rg, tell, email)){
+                        setClient({name,tell,login,email,rg,cpf,password,employer:clientType})
+                        console.log(client);
+                    }
+                    else{
+                        console.log("Dados invalidos");
+                    }
+                }} >CADASTRAR</button>
+                <button><Link to="/">VOLTAR</Link></button>
+               
+            </fieldset>
         </form>
-        </S.Container>
     );
 }
+
+export default RegisterPage;
--- frontend/bank-front/src/Services/api.js
+++ frontend/bank-front/src/Services/api.js
@@ -3,3 +3,20 @@ import axios from 'axios';
 export const fetchUserProfiles = () => {
     return axios.get("http://localhost:8080/clients");
 }
+
+export const validate = (login,password,cPassword,cpf,rg, tell, email) => {
+    if (login === '' || password === ''|| cPassword === '' || cpf === '' || rg === ''||tell === ''|| email ===''){
+        alert('PREENCHA OS CAMPOS DE MANEIRA CORRETA');
+        return false;
+    }else{
+        if (password === cPassword){
+            return true;
+        }
+        alert("AS SENHAS PRECISAM SER IGUAIS")
+        return false;  
+    }
+}
+
+export const postClient = (client) => {
+    return axios.post("http://localhost:8080/clients", client);
+}
