d54d2f63de503ccf22522089aeab04bcf8e23e2f
Guilherme Ilan <gui.tioze@gmail.com>
2021-01-15 15:26:44 -0300

    Corrigindo bugs

--- frontend/bank-front/src/Pages/LoginPage/index.js
+++ frontend/bank-front/src/Pages/LoginPage/index.js
@@ -4,8 +4,7 @@ import {Link} from 'react-router-dom'
 import { useEffect, useState } from 'react';
 
 
-<<<<<<< HEAD
-function LoginPage () {
+export default function LoginPage () {
     const [clients, setClients] = useState([]);
     const[login, setLogin] = useState('');
     const[password, setPassword] = useState('');
@@ -20,9 +19,6 @@ function LoginPage () {
 
     },[validando])
     
-=======
-export default function LoginPage () {
->>>>>>> 5c0efd2bb7fd8788f5cb6617194d0788fe905c95
     return(
         <S.Container>
             <S.Text>
@@ -30,7 +26,6 @@ export default function LoginPage () {
                 <span>Login Bank System</span>
             </S.Text>
             <S.Inputs>
-<<<<<<< HEAD
                 <input type="text" placeholder="login" value={login}  onChange={login =>setLogin(login.target.value)}/>
                 <input type="text" placeholder="Senha" value ={password} onChange={password => setPassword(password.target.value)}/>
                 <button type="button" onClick={()=>{
@@ -45,12 +40,6 @@ export default function LoginPage () {
                        }
                         
                     }} to={`${validando}`}>LOGIN</Link>
-=======
-                <input type="text" placeholder="Conta" />
-                <input type="text" placeholder="Senha" />
-                <button>
-                    <Link to="/homepageclient">LOGIN</Link>
->>>>>>> 5c0efd2bb7fd8788f5cb6617194d0788fe905c95
                 </button>
                 <button>
                     <Link to="/homepage">REGISTRAR</Link>
