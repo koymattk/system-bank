10dc2db38e28aa9132fe92edec0c2a55bdea88a3
Gabriel <koymattk@gmail.com>
2021-01-25 15:32:04 -0300

    Adicionando novas props

--- frontend/bank-front/src/Components/CardConfirm/index.js
+++ frontend/bank-front/src/Components/CardConfirm/index.js
@@ -3,15 +3,17 @@ import * as S from './styles';
 import {Link} from 'react-router-dom'
 
 
-export default function CardConfirm({nome, bank, value,typeAccount,numberAccount}){
+export default function CardConfirm({nome, bank, value,typeAccount,numberAccount,agency, dateTransfer, cpf}){
     return(
         <S.Container>
             <h3>Nome</h3>
             <span>{nome}</span>
             <h3>Banco</h3>
             <span>{bank}</span>
-            <h3>Valor</h3>
-            <span>{value}</span>
+            <h3>cpf</h3>
+            <span>{cpf}</span>
+            <h3>Data da tranferencia</h3>
+            <span>{dateTransfer}</span>
         </S.Container>
     )
 }
